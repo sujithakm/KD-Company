062b6a5fdffaa819006e51f9195cfd216a98d934
sujithakm <sujith@DESKTOP-LKT9RCQ>
2020-10-24 14:56:46 +0530

    Add project files.

---
+++ KD Company/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KD_Company.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Forlogin(string email,string password)
+        {
+            string Email = email;
+            string Password = password;
+            if (Email == "kd&company@gmail.com" && Password == "kdcompany")
+            {
+
+                return View();
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+    }
+}
