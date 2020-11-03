b3f1467a2350096a68a107231abab07be6007361
nitheeshkp24@gmail.com <nitheeshkp24@gmail.com>
2020-11-03 12:56:08 +0530

    Merge

--- KD Company/Models/UserAppDbContext.cs
+++ KD Company/Models/UserAppDbContext.cs
@@ -9,6 +9,7 @@ namespace KD_Company.Models
     public class UserAppDbContext:DbContext
     {
         public DbSet<UserDetails> userDetails { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
         public DbSet<CarDetails> cardetails { get; set; }
         public DbSet<Adminlogin> adminlogins { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
