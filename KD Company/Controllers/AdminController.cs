8614c1a70bcf5006a20f5095ccfc0a7c6da88327
sujithakm <sujith@DESKTOP-LKT9RCQ>
2020-11-04 12:13:32 +0530

    view notification design

--- KD Company/Controllers/AdminController.cs
+++ KD Company/Controllers/AdminController.cs
@@ -68,5 +68,11 @@ namespace KD_Company.Controllers
 
             return View(list);
         }
+        public IActionResult ViewnNotification()
+        {
+           
+
+            return View();
+        }
     }
 }
